8283b73ea385b00158ce1ca822eb0eb30b52a3ce
gaoyb7 <gaoyb7@gmail.com>
2016-01-06 23:15:34 +0800

    move js code after body element

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls', namespace='blog')),
     url(r'^accounts/login/$', auth_views.login),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
